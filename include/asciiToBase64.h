89237b061fa4ca8164ccf44a3412f0fdcb7b7254
Talha Jubair Siam <tjsiam019@gmail.com>
2023-12-23 21:19:38 +0600

    Complete upto Bitwise Operation

--- include/asciiToBase64.h
+++
@@ -1,6 +0,0 @@
-#ifndef ASCIITOBASE64_H
-#define ASCIITOBASE64_H
-
-char *asciiToBase64(const char *input_str);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,14 +1,57 @@
 #include <gtk/gtk.h>
 #include "rot.h"
-#include "asciiToBase64.h"
 #include "baseConverter.h"
 #include "analyzeText.h"
 #include "reverseString.h"
 #include "runLengthEncoding.h"
 #include "caseConversion.h"
 #include "morseCodeEncoding.h"
+#include "urlEncode.h"
+#include "urlDecode.h"
+
 
 GtkBuilder *builder;
+
+void on_base_conversion_input_changed(){
+    GtkEntry *base_conversion_input = GTK_ENTRY(gtk_builder_get_object(builder, "base_conversion_text_input"));
+    GtkEntry *base_conversion_output = GTK_ENTRY(gtk_builder_get_object(builder, "base_conversion_text_output"));
+    GtkComboBoxText *base_conversion_from = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "base_conversion_combo_input"));
+    GtkComboBoxText *base_conversion_to = GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "base_conversion_combo_output"));
+
+    const char *input_text_const = gtk_entry_get_text(base_conversion_input);
+    char *input_text = strdup(input_text_const); 
+
+    char *from = gtk_combo_box_text_get_active_text(base_conversion_from);
+    char *to = gtk_combo_box_text_get_active_text(base_conversion_to);
+
+    Converter converter;
+
+    initConverter(&converter, input_text, from);
+
+    char *output_text;
+
+    if (strcmp(to, "decimal") == 0) {
+        output_text = converter.toDecimal(&converter);
+    } else if (strcmp(to, "binary") == 0) {
+        output_text = converter.toBinary(&converter);
+    } else if (strcmp(to, "octal") == 0) {
+        output_text = converter.toOctal(&converter);
+    } else if (strcmp(to, "hexadecimal") == 0) {
+        output_text = converter.toHexadecimal(&converter);
+    }
+
+
+    gtk_entry_set_text(base_conversion_output, output_text);
+
+    // free(input_text);
+
+    // if (output_text != input_text) { 
+    //     free(output_text);
+    // }
+
+    // g_free(from); 
+    // g_free(to); 
+}
 
 void on_textanalyzer_file_set(){
     char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(gtk_builder_get_object(builder, "textanalyzer_file_chooser")));
@@ -132,6 +175,26 @@ void on_rot_input_changed(){
 
     gtk_entry_set_text(rot_output, output_text);
 
+}
+
+void on_url_encode_input_changed(){
+    GtkEntry *url_encode_input = GTK_ENTRY(gtk_builder_get_object(builder, "url_encode_input"));
+    GtkEntry *url_encode_output = GTK_ENTRY(gtk_builder_get_object(builder, "url_encode_output"));
+
+    char *input_text = gtk_entry_get_text(url_encode_input);
+    char *output_text = urlEncode(input_text);
+
+    gtk_entry_set_text(url_encode_output, output_text);
+}
+
+void on_url_decode_input_changed(){
+    GtkEntry *url_decode_input = GTK_ENTRY(gtk_builder_get_object(builder, "url_decode_input"));
+    GtkEntry *url_decode_output = GTK_ENTRY(gtk_builder_get_object(builder, "url_decode_output"));
+
+    char *input_text = gtk_entry_get_text(url_decode_input);
+    char *output_text = urlDecode(input_text);
+
+    gtk_entry_set_text(url_decode_output, output_text);
 }
 
 int main(int argc, char *argv[])
